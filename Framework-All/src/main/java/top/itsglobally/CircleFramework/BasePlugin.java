1958a76f1321db34dce0f056a2873c59f2efebb6
ItsGlobally <itsglobally1324@gmail.com>
2026-03-11 20:58:49 +0800

    e

--- Framework-All/src/main/java/top/itsglobally/CircleFramework/BasePlugin.java
+++ Framework-All/src/main/java/top/itsglobally/CircleFramework/BasePlugin.java
@@ -1,4 +1,36 @@
 package top.itsglobally.CircleFramework;
 
-public class BasePlugin {
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public abstract class BasePlugin<T extends BasePlugin> extends JavaPlugin {
+
+    private static BasePlugin<?> instance;
+
+    @Override
+    public void onLoad() {
+        this.load();
+    }
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        this.enable();
+    }
+
+    @Override
+    public void onDisable() {
+        this.disable();
+
+    }
+    protected void load() {}
+
+    protected abstract void enable();
+
+    protected abstract void disable();
+
+    public static <T extends BasePlugin<?>> T getInstance() {
+        return (T) instance;
+    }
+
 }
